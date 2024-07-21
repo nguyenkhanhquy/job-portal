b9b3066196b2d7fc9c3cad9028386bbacfc84fe3
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-07-21 21:53:41 +0700

    add: entity Users and UsersType

---
+++ src/main/java/com/jobportal/entity/UsersType.java
@@ -0,0 +1,34 @@
+package com.jobportal.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users_type")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UsersType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int userTypeId;
+
+    private String userTypeName;
+
+    @OneToMany(targetEntity = Users.class, mappedBy = "userTypeId" , cascade = CascadeType.ALL)
+    private List<Users> users;
+
+    // Only select these two entries
+    // This will avoid recursive toString() issue with UsersType <-> Users
+    @Override
+    public String toString() {
+        return "UsersType{" +
+                "userTypeName='" + userTypeName + '\'' +
+                ", userTypeId=" + userTypeId +
+                '}';
+    }
+}
