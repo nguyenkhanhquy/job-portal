30605b347a9db2f301cbfa126f643564984fb9e8
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-07-22 08:42:58 +0700

    add: UsersService

--- src/main/java/com/jobportal/controller/UsersController.java
+++ src/main/java/com/jobportal/controller/UsersController.java
@@ -2,21 +2,26 @@ package com.jobportal.controller;
 
 import com.jobportal.entity.Users;
 import com.jobportal.entity.UsersType;
+import com.jobportal.service.UsersService;
 import com.jobportal.service.UsersTypeService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
 @Controller
 public class UsersController {
 
+    private final UsersService usersService;
     private final UsersTypeService usersTypeService;
 
     @Autowired
-    public UsersController(UsersTypeService usersTypeService) {
+    public UsersController(UsersService usersService, UsersTypeService usersTypeService) {
+        this.usersService = usersService;
         this.usersTypeService = usersTypeService;
     }
 
@@ -26,5 +31,12 @@ public class UsersController {
         model.addAttribute("getAllTypes", usersTypes);
         model.addAttribute("user", new Users());
         return "register";
+    }
+
+    @PostMapping("/register/new")
+    public String userRegistration(@Valid Users user) {
+        //System.out.println("User:: " + user);
+        usersService.addNew(user);
+        return "dashboard";
     }
 }
---
+++ src/main/java/com/jobportal/service/UsersService.java
@@ -0,0 +1,25 @@
+package com.jobportal.service;
+
+import com.jobportal.entity.Users;
+import com.jobportal.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class UsersService {
+
+    private final UsersRepository usersRepository;
+
+    @Autowired
+    public UsersService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    public Users addNew(Users user) {
+        user.setActive(true);
+        user.setRegistrationDate(new Date(System.currentTimeMillis()));
+        return usersRepository.save(user);
+    }
+}
