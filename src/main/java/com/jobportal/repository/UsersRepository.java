f70a472c212694152b5a6a48a0226bd73e4b8ae5
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-07-22 08:12:52 +0700

    add: Repositories, Services and Controller
    - Users
    - UsersType

---
+++ src/main/java/com/jobportal/repository/UsersRepository.java
@@ -0,0 +1,8 @@
+package com.jobportal.repository;
+
+import com.jobportal.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersRepository extends JpaRepository<Users, Integer> {
+
+}
