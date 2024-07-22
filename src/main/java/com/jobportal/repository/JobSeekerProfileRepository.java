c345c46b7b9206af7709683f75961853da65e1bc
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-07-22 10:23:45 +0700

    add: repository RecruiterProfile and JobSeekerProfile

---
+++ src/main/java/com/jobportal/repository/JobSeekerProfileRepository.java
@@ -0,0 +1,8 @@
+package com.jobportal.repository;
+
+import com.jobportal.entity.JobSeekerProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobSeekerProfileRepository extends JpaRepository<JobSeekerProfile, Integer> {
+
+}
---
+++ src/main/java/com/jobportal/repository/RecruiterProfileRepository.java
@@ -0,0 +1,8 @@
+package com.jobportal.repository;
+
+import com.jobportal.entity.RecruiterProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecruiterProfileRepository extends JpaRepository<RecruiterProfile, Integer> {
+
+}
