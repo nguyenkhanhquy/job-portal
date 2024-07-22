7071bff480513bc25c1c773c1831653c25f3470b
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-07-22 12:37:28 +0700

    add: custom authentication success handler

--- src/main/java/com/jobportal/config/WebSecurityConfig.java
+++ src/main/java/com/jobportal/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,10 +16,13 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WebSecurityConfig {
 
     private final CustomUserDetailsService customUserDetailsService;
+    private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
 
     @Autowired
-    public WebSecurityConfig(CustomUserDetailsService customUserDetailsService) {
+    public WebSecurityConfig(CustomUserDetailsService customUserDetailsService,
+                             CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler) {
         this.customUserDetailsService = customUserDetailsService;
+        this.customAuthenticationSuccessHandler = customAuthenticationSuccessHandler;
     }
 
     private final String[] publicUrl = {"/",
@@ -38,10 +42,20 @@ public class WebSecurityConfig {
 
     protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authenticationProvider(authenticationProvider());
+
         http.authorizeHttpRequests(auth->{
             auth.requestMatchers(publicUrl).permitAll();
             auth.anyRequest().authenticated();
         });
+
+        http.formLogin(from -> from.loginPage("/login")
+                        .permitAll()
+                        .successHandler(customAuthenticationSuccessHandler))
+                .logout(logout -> {
+                    logout.logoutUrl("/logout");
+                    logout.logoutSuccessUrl("/");
+                }).cors(Customizer.withDefaults()).csrf(csrf -> csrf.disable());
+
         return http.build();
     }
 
--- src/main/java/com/jobportal/service/UsersService.java
+++ src/main/java/com/jobportal/service/UsersService.java
@@ -7,6 +7,7 @@ import com.jobportal.repository.JobSeekerProfileRepository;
 import com.jobportal.repository.RecruiterProfileRepository;
 import com.jobportal.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -18,19 +19,23 @@ public class UsersService {
     private final UsersRepository usersRepository;
     private final RecruiterProfileRepository recruiterProfileRepository;
     private final JobSeekerProfileRepository jobSeekerProfileRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
     public UsersService(UsersRepository usersRepository,
                         RecruiterProfileRepository recruiterProfileRepository,
-                        JobSeekerProfileRepository jobSeekerProfileRepository) {
+                        JobSeekerProfileRepository jobSeekerProfileRepository,
+                        PasswordEncoder passwordEncoder) {
         this.usersRepository = usersRepository;
         this.recruiterProfileRepository = recruiterProfileRepository;
         this.jobSeekerProfileRepository = jobSeekerProfileRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Users addNew(Users user) {
         user.setActive(true);
         user.setRegistrationDate(new Date(System.currentTimeMillis()));
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         Users savedUser = usersRepository.save(user);
         int userTypeId = user.getUserTypeId().getUserTypeId();
         if (userTypeId == 1) {
