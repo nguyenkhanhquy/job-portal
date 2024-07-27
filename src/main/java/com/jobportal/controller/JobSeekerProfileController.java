706eafab3958463c071d022902d2f975a9bc0821
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-07-27 22:26:45 +0700

    add: support for file upload Job Seeker profile image and resumé

--- src/main/java/com/jobportal/controller/JobSeekerProfileController.java
+++ src/main/java/com/jobportal/controller/JobSeekerProfileController.java
@@ -5,18 +5,23 @@ import com.jobportal.entity.Skills;
 import com.jobportal.entity.Users;
 import com.jobportal.repository.UsersRepository;
 import com.jobportal.service.JobSeekerProfileService;
-import com.jobportal.service.UsersService;
+import com.jobportal.util.FileUploadUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Controller
@@ -46,8 +51,10 @@ public class JobSeekerProfileController {
             Optional<JobSeekerProfile> seekerProfile = jobSeekerProfileService.getOne(user.getUserId());
             if (seekerProfile.isPresent()) {
                 jobSeekerProfile = seekerProfile.get();
-                jobSeekerProfile.setSkills(skills);
-
+                if (jobSeekerProfile.getSkills().isEmpty()) {
+                    skills.add(new Skills());
+                    jobSeekerProfile.setSkills(skills);
+                }
             }
 
             model.addAttribute("skills", skills);
@@ -55,5 +62,57 @@ public class JobSeekerProfileController {
         }
 
         return "job-seeker-profile";
+    }
+
+    @PostMapping("/addNew")
+    public String addNew(JobSeekerProfile jobSeekerProfile,
+                         @RequestParam("image") MultipartFile image,
+                         @RequestParam("pdf") MultipartFile pdf,
+                         Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            String currentUsername = authentication.getName();
+            Users user = usersRepository.findByEmail(currentUsername).orElseThrow(
+                    () -> new RuntimeException("Could not found user"));
+            jobSeekerProfile.setUserId(user);
+            jobSeekerProfile.setUserAccountId(user.getUserId());
+        }
+
+        List<Skills> skills = new ArrayList<>();
+        model.addAttribute("profile", jobSeekerProfile);
+        model.addAttribute("skills", skills);
+
+        for (Skills skill : jobSeekerProfile.getSkills()) {
+            skill.setJobSeekerProfile(jobSeekerProfile);
+        }
+
+        String imageName = "";
+        String resumeName = "";
+
+        if (!Objects.equals(image.getOriginalFilename(), "")) {
+            imageName = StringUtils.cleanPath(Objects.requireNonNull(image.getOriginalFilename()));
+            jobSeekerProfile.setProfilePhoto(imageName);
+        }
+
+        if (!Objects.equals(pdf.getOriginalFilename(), "")) {
+            resumeName = StringUtils.cleanPath(Objects.requireNonNull(pdf.getOriginalFilename()));
+            jobSeekerProfile.setResume(resumeName);
+        }
+
+        JobSeekerProfile savedUser = jobSeekerProfileService.addNew(jobSeekerProfile);
+        String uploadDir = "photos/candidate/" + savedUser.getUserAccountId();
+
+        try {
+            if (!Objects.equals(image.getOriginalFilename(), "")) {
+                FileUploadUtil.saveFile(uploadDir, imageName, image);
+            }
+            if (!Objects.equals(pdf.getOriginalFilename(), "")) {
+                FileUploadUtil.saveFile(uploadDir, resumeName, pdf);
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException("Could not save file", ex);
+        }
+
+        return "redirect:/dashboard/";
     }
 }
