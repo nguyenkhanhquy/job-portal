65097b97be7a4bbfeb5f1b4ba9b8f538026716bb
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-07-27 19:08:17 +0700

    feat: display details and edit a job

--- src/main/java/com/jobportal/service/JobPostActivityService.java
+++ src/main/java/com/jobportal/service/JobPostActivityService.java
@@ -33,4 +33,8 @@ public class JobPostActivityService {
         }
         return recruiterJobsDtoList;
     }
+
+    public JobPostActivity getOne(int id) {
+        return jobPostActivityRepository.findById(id).orElseThrow(() -> new RuntimeException("Job not found"));
+    }
 }
